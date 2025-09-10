9a7a19f39af152790dea31920540578925f1e38a
Victor Vahid <victorvahid76@gmail.com>
2025-09-10 13:44:33 -0400

    fix: Correção de erro no userCase

--- src/useCases/User.js
+++ src/useCases/User.js
@@ -3,31 +3,31 @@ const User = require("../models/userModel");
 const User = require("../models/userModel");
 
 class UserUseCase {
-  constructor(validator) {
-    this.validator = validator;
-  }
+	constructor(validator) {
+		this.validator = validator;
+	}
 
-  async getUserById(userId) {
-    return await User.findById(userId);
-  }
+	async getUserById(userId) {
+		return await User.findById(userId);
+	}
 
-  async createUser(userData) {
-    const { error } = this.validator.validate(userData);
-    if (error) throw new Error(error.details[0].message);
-    return await User.create(userData);
-  }
+	async createUser(userData) {
+		const { error } = this.validator.validate(userData);
+		if (error) throw new Error(error.details[0].message);
+		return await User.create(userData);
+	}
 
-  async updateUser(userId, updateData) {
-    return await User.findByIdAndUpdate(userId, updateData, { new: true });
-  }
+	async updateUser(userId, updateData) {
+		return await User.findByIdAndUpdate(userId, updateData, { new: true });
+	}
 
-  async deleteUser(userId) {
-    return await User.findByIdAndDelete(userId);
-  }
+	async deleteUser(userId) {
+		return await User.findByIdAndDelete(userId);
+	}
 
-  async listUsers(filter = {}) {
-    return await User.find(filter);
-  }
+	async listUsers(filter = {}) {
+		return await User.find(filter);
+	}
 }
 
 module.exports = UserUseCase;
